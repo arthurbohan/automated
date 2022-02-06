f52e46526a57d4bd27a66581cd49f59f02bbe3bd
Arthur <arthurbokhanqa@gmail.com>
2022-02-06 20:41:27 +0300

    rewrite creditionals

--- test/specs/tryForKarma.js
+++ test/specs/tryForKarma.js
@@ -2,8 +2,8 @@ describe('Login form', () => {
     it('Verify login', async () => {
         browser.url('https://www.karmanow.com/');
         await $("button[type='button']").click();
-        const username = 'ddd@ddd.com',
-            password = 'ddd@ddd.com';
+        const username = 'LLlkaqp@test.com',
+            password = 'LLlkaqp@test.com';
         await $("#email").setValue(username);
         await $("#password").setValue(password);
         await $("button[type='submit']").click();
