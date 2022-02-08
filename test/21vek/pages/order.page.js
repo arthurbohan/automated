ced90d6b8e212e63206710c4fe8eb75794e514ef
Arthur <arthurbokhanqa@gmail.com>
2022-02-09 01:00:50 +0300

    add selectors to order page

--- test/21vek/pages/order.page.js
+++ test/21vek/pages/order.page.js
@@ -3,6 +3,18 @@ class OrderPage {
         return $("//button[contains(@class, 'cr-button__order')]");
     }
 
+    get nameField(){
+        return $("input[name='data[private][name]'");
+    }
+
+    get emailField(){
+        return $("input[name='data[private][email]'");
+    }
+
+    get cityField(){
+        return $("input[name='data[city]'");
+    }
+    
     async clickCheckoutButton() {
         await this.checkoutButton.click();
     }
